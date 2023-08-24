49079f0a617f0638c7cc768bbeeeaf0c7d0412ac
d f <tfwookev@gmail.com>
2023-08-24 01:34:43 -0400

    Test function for ShowState

--- ShowState.py
+++ ShowState.py
@@ -1,3 +1,11 @@
 from pymongo import MongoClient
 
 class ShowState():
+    client = MongoClient("localhost", "27017")
+    db = client.Loambot
+    showsCollection = db.Shows
+
+
+    def getAllShows(self):
+        for show in self.showsCollection:
+            print(show)
